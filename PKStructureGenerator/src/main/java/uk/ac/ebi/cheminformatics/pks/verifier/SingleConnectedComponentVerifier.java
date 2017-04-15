5f985fafa92078a6bbad5e7f644c9e4f7cef11d5
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-04-15 01:08:38 +0100

    Debugging docker issue with bond 0

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/SingleConnectedComponentVerifier.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/SingleConnectedComponentVerifier.java
@@ -1,7 +1,10 @@
 package uk.ac.ebi.cheminformatics.pks.verifier;
 
+import org.apache.log4j.Logger;
 import org.openscience.cdk.graph.ConnectivityChecker;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
 import org.openscience.cdk.interfaces.IBond;
 import uk.ac.ebi.cheminformatics.pks.generator.PKStructure;
 
@@ -13,10 +16,27 @@ import uk.ac.ebi.cheminformatics.pks.generator.PKStructure;
  * To change this template use File | Settings | File Templates.
  */
 public class SingleConnectedComponentVerifier implements Verifier {
+
+    private static final Logger LOGGER = Logger.getLogger(SingleConnectedComponentVerifier.class);
+
     @Override
     public boolean verify(PKStructure struc) {
         IAtomContainer mol = struc.getMolecule();
-        return !ConnectivityChecker.isConnected(mol);
+        for(IBond bond : mol.bonds()) {
+            for(IAtom atomInBond : bond.atoms()) {
+                if(!mol.contains(atomInBond)) {
+                    LOGGER.error(
+                            "Atom "+atomInBond.getSymbol()+" "+atomInBond.getID()+" not part of molecule. Bond "+mol.getBondNumber(bond));
+                }
+            }
+        }
+        if(!ConnectivityChecker.isConnected(mol)) {
+            IAtomContainerSet mols = ConnectivityChecker.partitionIntoMolecules(mol);
+                    LOGGER.error("Number of different components: "+mols.getAtomContainerCount());
+            System.out.println("Number of different components: "+mols.getAtomContainerCount());
+            return true;
+        }
+        return false;
     }
 
     @Override
